03e21fda5e0688adeddf8238731917cf4cd3b8e6
Neilton Souza <neilton.sds1991@gmail.com>
2021-07-18 20:07:32 -0300

    primeiro commit

---
+++ repeticoes.js
@@ -0,0 +1,29 @@
+//EXEMPLO 1
+/*var colors= ["black", "white", "yellow","green", "blue"];
+for (var i = 0;i<colors.length;i++){
+    console.log(colors[i]);
+}*/
+
+//EMEMPLO 2
+/*var colors= ["black", "white", "yellow","green", "blue"];
+for (var i = 0;i<5;i++){
+    console.log(colors[i]);
+}*/
+
+//EXEMPLO 3
+/*for (var i=1; i<=10; i++ ){
+    console.log(i);
+}*/
+
+//EXEMPLO 4
+/*var i = 1;
+while (i<=10){
+    console.log(i);
+    i++;
+}*/
+
+var i = 1;
+do{
+    console.log(i);
+    i++;
+}while(i<10);
